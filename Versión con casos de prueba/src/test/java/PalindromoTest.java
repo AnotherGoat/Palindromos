57b50484090068d8df2003a6bc5364090073de52
VictorMardones <61598238+VictorMardones@users.noreply.github.com>
2020-05-17 20:18:30 -0400

    Añadida Versión con casos de prueba

---
+++ Versión con casos de prueba/src/test/java/PalindromoTest.java
@@ -0,0 +1,56 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PalindromoTest {
+
+    Palindromo p;
+
+    @Before
+    public void setUp() throws Exception {
+        p = new Palindromo();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        p = null;
+    }
+
+    // 5 casos de prueba propuestos por mi
+    @Test
+    public void esPalindromo0() {
+        assertTrue(p.esPalindromo("anitalavalatina"));
+    }
+    @Test
+    public void esPalindromo1() {
+        assertFalse(p.esPalindromo("AnitaLavaLaTina"));
+    }
+    @Test
+    public void esPalindromo2() {
+        assertFalse(p.esPalindromo("anita lava la tina"));
+    }
+    @Test
+    public void esPalindromo3() {
+        assertTrue(p.esPalindromo("010010"));
+    }
+    @Test
+    public void esPalindromo4() {
+        assertTrue(p.esPalindromo("%#@#%"));
+    }
+
+    // "aca", "acas", "h"
+    @Test
+    public void esPalindromo5() {
+        assertTrue(p.esPalindromo("aca"));
+    }
+    @Test
+    public void esPalindromo6() {
+        assertFalse(p.esPalindromo("acas"));
+    }
+    @Test
+    public void esPalindromo7() {
+        assertTrue(p.esPalindromo("h"));
+    }
+}
