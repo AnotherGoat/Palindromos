cd0411b8863eb974830f5503f8b91a477f564c01
VictorMardones <61598238+VictorMardones@users.noreply.github.com>
2020-05-17 21:28:10 -0400

    Reemplaza mayúsculas por minúsculas

--- Versión final/src/main/java/Palindromo.java
+++ Versión final/src/main/java/Palindromo.java
@@ -19,8 +19,16 @@ public class Palindromo {
     
     private String revisarString(String a){
 
+        // Elimina los espacios
         a = a.replace(" ", "");
         System.out.println(a);
+
+        // Reemplaza mayúsculas por minúsculas
+        String[] mayusculas = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        String[] minusculas = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        for(int i=0; i<mayusculas.length; i++){
+            a = a.replace(mayusculas[i], minusculas[i]);
+        }
 
         return a;
     }
